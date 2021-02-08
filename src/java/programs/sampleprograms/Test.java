2f2eea2d2b5e5ebe712fbb9a9355cc5eb3bbb2a6
kiran <kirantyga@gmail.com>
2021-02-08 14:14:43 -0500

    revised changes

--- src/java/programs/sampleprograms/Test.java
+++ src/java/programs/sampleprograms/Test.java
@@ -2,4 +2,10 @@ package java.programs.sampleprograms;
 
 public class Test {
 
+<<<<<<< HEAD
+=======
+	public static void main(String[] args) {
+		System.out.println("Hello child1");
+	}
+>>>>>>> parent of b3fafed (child1-commit2)
 }
